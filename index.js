42265f492d93ae564c4c264f32c89e0444c79ad0
MohammedJunaid0786 <147134124+MohammedJunaid0786@users.noreply.github.com>
2023-11-07 22:50:50 +0530

    Update index.js

--- index.js
+++ index.js
@@ -38,75 +38,6 @@ typeEffect();
 
 
 
-
-
-
-
-var tablinks = document.getElementsByClassName("tab-links");
-var tabcontents = document.getElementsByClassName("tab-contents");
-
-function opentab(tabname){
-    for(tablink of tablinks){
-        tablink.classList.remove("active-link");
-    }
-    for(tabcontent of tabcontents){
-        tabcontent.classList.remove("active-tab");
-    }
-    event.currentTarget.classList.add("active-link");
-    document.getElementById(tabname).classList.add("active-tab");
-}
-
-
-var sidemeu = document.getElementById("sidemenu");
-
-function openmenu(){
-    sidemeu.style.right = "0";
-}
-
-function closemenu(){
-    sidemeu.style.right = "-200px";
-}
-
-
-
-
-const dynamicText = document.querySelector("h2 span")
-const words = ["Love", "Like Art", "the Future", "Everything"];
-
-let wordIndex = 0;
-let charIndex = 0;
-let isDeleting = false;
-
-const typeEffect = () => {
-    const currentWord = words[wordIndex];
-    const currentChar = currentWord.substring(0, charIndex);
-    dynamicText.textContent = currentChar;
-    dynamicText.classList.add("stop-blinking");
-
-    if(!isDeleting && charIndex < currentWord.length) {
-
-        charIndex++;
-        setTimeout(typeEffect, 200);
-    }else if(isDeleting && charIndex > 0) {
-        charIndex--;
-        setTimeout(typeEffect, 100);
-    } else{
-        isDeleting = !isDeleting;
-        dynamicText.classList.remove("stop-blinking");
-        wordIndex = !isDeleting ? (wordIndex + 1) % words.length : wordIndex;
-        setTimeout(typeEffect, 1200)
-    }
-
-}
-
-typeEffect();
-
-
-
-
-
-
-
 var tablinks = document.getElementsByClassName("tab-links");
 var tabcontents = document.getElementsByClassName("tab-contents");
 
@@ -202,3 +133,20 @@ const initSlider = () => {
 
 window.addEventListener("resize", initSlider);
 window.addEventListener("load", initSlider);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
